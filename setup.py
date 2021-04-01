8eba1fec9f57af9e362afc93d7aa1e9f613b2591
Tom Aitken <tom@shop-grok.com>
2021-04-02 00:43:15 +1100

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='scrapy-testmaster',
-    version='1.1',
+    version='1.2',
     author='Thomas Aitken',
     author_email='tclaitken@gmail.com',
     description='Automated testing and debugging tool for Scrapy.',
